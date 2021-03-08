680fb9f0795f36b92536468cdbe76e88f48ab56b
Ritik Jr <80249193+Ritik-Swami-Jr@users.noreply.github.com>
2021-03-08 14:34:07 +0530

    Add files via upload

---
+++ costumelements.js
@@ -0,0 +1,6 @@
+document.createElement('main-layout');
+document.createElement('nav-logo');
+document.createElement('nav-items');
+document.createElement('pro-about');
+document.createElement('pro-contact');
+document.createElement('pro-gallery');
---
+++ script.js
@@ -0,0 +1,15 @@
+function nav_more_tools() {
+    document.getElementById('nav-more-tools').style.display='block';
+}
+function nav_exit_tools() {
+    document.getElementById('nav-more-tools').style.display='none';
+}
+window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll <= 100) {
+      opacity = 0;
+    } else {
+      opacity = 1;
+    }
+    document.querySelector("#call-now").style.opacity = opacity;
+  });
