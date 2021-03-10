f3f495236543bfbe0f442642ada327d88e971747
Ritik RS <ritikswami400@gmail.com>
2021-03-10 12:25:17 +0530

    Added data.json and test.html

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ window.addEventListener("scroll", () => {
     }
     document.querySelector("#call-now").style.opacity = opacity;
   });
+
+  // var mydata = JSON.parse(data);
+  
+  // alert(mydata[0].name);
+
+// fetch("data.json")
+//   .then(response => response.json())
+//   .then(json => demo.innerHTML = json[0].name);
+  
+// Replace ./data.json with your JSON feed
+fetch('data.json').then(response => {
+  return response.json();
+}).then(data => {
+  // Work with JSON data here
+  var name = document.getElementById('demo');
+  console.log(data[0].name);
+  name.innerHTML = data[0].name;
+  console.log(data);
+}).catch(err => {
+  // Do something for an error here
+  console.log('Error')
+});
